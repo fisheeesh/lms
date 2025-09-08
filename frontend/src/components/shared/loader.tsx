f17a0c68c253d631a824e774d69da20f1ff5d667
fisheeesh <swanphyo444@gmail.com>
2025-09-09 01:17:26 +0700

    chore: created app loader for better ux

---
+++ frontend/src/components/shared/loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className="absolute bg-card/90 backdrop-blur-sm z-[999] inset-0 flex items-center justify-center">
+            <div className="loader"></div>
+        </div>
+    )
+}
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,10 @@
-import { Loader } from 'lucide-react'
 import { Suspense } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import LoginPage from './pages/auth/login-page'
 import DashboardRootLayout from './pages/dashboard/dashboard-root-layout'
 import ErrorElement from './pages/not-found/error-element'
 import NotFound from './pages/not-found/not-found'
+import Loader from './components/shared/loader'
 
 export default function Router() {
     const router = createBrowserRouter([
