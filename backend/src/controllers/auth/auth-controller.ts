9359590957fcf4c2dcfa5e3cf064e803685dad6e
fisheeesh <swanphyo444@gmail.com>
2025-09-09 00:49:56 +0700

    feat: verify otp route created

--- backend/src/controllers/auth/auth-controller.ts
+++ backend/src/controllers/auth/auth-controller.ts
@@ -98,107 +98,107 @@ export const register = [
     }
 ]
 
-// export const verifyOtp = [
-//     body("email", "Invalid email format.")
-//         .trim()
-//         .notEmpty()
-//         .isEmail(),
-//     body("otp", "Invalid OTP.")
-//         .trim()
-//         .notEmpty()
-//         .matches(/^[\d]+$/)
-//         .isLength({ min: 6, max: 6 }),
-//     body('token', "Invalid Token.")
-//         .trim()
-//         .notEmpty()
-//         .escape(),
-//     async (req: Request, res: Response, next: NextFunction) => {
-//         const errors = validationResult(req).array({ onlyFirstError: true })
-//         if (errors.length > 0) return next(createHttpError({
-//             message: errors[0].msg,
-//             status: 400,
-//             code: errorCodes.invalid,
-//         }))
+export const verifyOtp = [
+    body("email", "Invalid email format.")
+        .trim()
+        .notEmpty()
+        .isEmail(),
+    body("otp", "Invalid OTP.")
+        .trim()
+        .notEmpty()
+        .matches(/^[\d]+$/)
+        .isLength({ min: 6, max: 6 }),
+    body('token', "Invalid Token.")
+        .trim()
+        .notEmpty()
+        .escape(),
+    async (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req).array({ onlyFirstError: true })
+        if (errors.length > 0) return next(createHttpError({
+            message: errors[0].msg,
+            status: 400,
+            code: errorCodes.invalid,
+        }))
 
-//         const { email, otp, token } = req.body
-//         const user = await getUserByEmail(email)
-//         checkUserExit(user)
+        const { email, otp, token } = req.body
+        const user = await getUserByEmail(email)
+        checkUserExit(user)
 
-//         const otpRow = await getOTPByEmail(email)
-//         checkOTPRow(otpRow)
+        const otpRow = await getOTPByEmail(email)
+        checkOTPRow(otpRow)
 
-//         const lastOtpVerify = new Date(otpRow!.updatedAt).toLocaleDateString()
-//         const today = new Date().toLocaleDateString()
-//         const isSameDate = lastOtpVerify === today
-//         //* If OTP verify is in the same date and over limit
-//         checkOTPErrorIfSameDate(isSameDate, otpRow!.error)
+        const lastOtpVerify = new Date(otpRow!.updatedAt).toLocaleDateString()
+        const today = new Date().toLocaleDateString()
+        const isSameDate = lastOtpVerify === today
+        //* If OTP verify is in the same date and over limit
+        checkOTPErrorIfSameDate(isSameDate, otpRow!.error)
 
-//         if (otpRow?.rememberToken !== token) {
-//             const otpData = {
-//                 error: 5
-//             }
-//             await updateOTP(otpRow!.id, otpData)
+        if (otpRow?.rememberToken !== token) {
+            const otpData = {
+                error: 5
+            }
+            await updateOTP(otpRow!.id, otpData)
 
-//             return next(createHttpError({
-//                 message: 'Invalid Token.',
-//                 status: 400,
-//                 code: errorCodes.invalid,
-//             }))
-//         }
+            return next(createHttpError({
+                message: 'Invalid Token.',
+                status: 400,
+                code: errorCodes.invalid,
+            }))
+        }
 
-//         const isExpired = moment().diff(otpRow!.updatedAt) > 2 * 60 * 1000;
+        const isExpired = moment().diff(otpRow!.updatedAt) > 2 * 60 * 1000;
 
-//         //* Check if otp is expired
-//         if (isExpired) return next(createHttpError({
-//             message: 'OTP is expired. Please try again.',
-//             status: 403,
-//             code: errorCodes.otpExpired,
-//         }))
+        //* Check if otp is expired
+        if (isExpired) return next(createHttpError({
+            message: 'OTP is expired. Please try again.',
+            status: 403,
+            code: errorCodes.otpExpired,
+        }))
 
 
-//         const isMatchOtp = await bcrypt.compare(otp, otpRow!.otp)
+        const isMatchOtp = await bcrypt.compare(otp, otpRow!.otp)
 
-//         //* Check if otp is match
-//         if (!isMatchOtp) {
-//             //* If OTP is first time today
-//             if (!isSameDate) {
-//                 const otpData = {
-//                     error: 1
-//                 }
+        //* Check if otp is match
+        if (!isMatchOtp) {
+            //* If OTP is first time today
+            if (!isSameDate) {
+                const otpData = {
+                    error: 1
+                }
 
-//                 await updateOTP(otpRow!.id, otpData)
-//             } else {
-//                 //* If OTP error is not first time today
-//                 const otpData = {
-//                     error: { increment: 1 }
-//                 }
+                await updateOTP(otpRow!.id, otpData)
+            } else {
+                //* If OTP error is not first time today
+                const otpData = {
+                    error: { increment: 1 }
+                }
 
-//                 await updateOTP(otpRow!.id, otpData)
-//             }
+                await updateOTP(otpRow!.id, otpData)
+            }
 
-//             return next(createHttpError({
-//                 message: 'OTP is incorrect.',
-//                 status: 400,
-//                 code: errorCodes.invalid,
-//             }))
-//         }
+            return next(createHttpError({
+                message: 'OTP is incorrect.',
+                status: 400,
+                code: errorCodes.invalid,
+            }))
+        }
 
-//         const verifyToken = generateToken()
-//         const otpData = {
-//             verifyToken,
-//             error: 0,
-//             count: 1
-//         }
+        const verifyToken = generateToken()
+        const otpData = {
+            verifyToken,
+            error: 0,
+            count: 1
+        }
 
-//         const result = await updateOTP(otpRow!.id, otpData)
+        const result = await updateOTP(otpRow!.id, otpData)
 
-//         res.status(200).json({
-//             message: "OTP is successfully verified.",
-//             email: result.email,
-//             token: result.verifyToken
-//         })
-//     }
-// ]
+        res.status(200).json({
+            message: "OTP is successfully verified.",
+            email: result.email,
+            token: result.verifyToken
+        })
+    }
+]
 
 // export const confirmPassword = [
 //     body("email", "Invalid email format.")
