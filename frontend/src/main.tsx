e9d443899928079f98630af3ac8425ee7bf84a05
fisheeesh <swanphyo444@gmail.com>
2025-09-10 00:19:45 +0700

    chore: axios instance created and added interceptors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './routes'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './api/query'
+import { Toaster } from 'sonner'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from '@/components/shared/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
-  <Router />
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-left' />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router />
+      <Toaster richColors />
+    </ThemeProvider>
+  </QueryClientProvider>
 )
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,18 @@
 import { Suspense } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import Loader from './components/shared/loader'
+import ConfirmPasswordPage from './pages/auth/confirm-password-page'
+import ForgotPasswordPage from './pages/auth/forgot-password-page'
 import LoginPage from './pages/auth/login-page'
+import RegisterPage from './pages/auth/register-page'
+import ResetPasswordPage from './pages/auth/reset-password-page'
+import VerifyForgotOTPPage from './pages/auth/verify-forgot-otp-page'
+import VerifyOTPPage from './pages/auth/verify-otp-page'
 import DashboardRootLayout from './pages/dashboard/dashboard-root-layout'
 import ErrorElement from './pages/not-found/error-element'
-import NotFound from './pages/not-found/not-found'
-import Loader from './components/shared/loader'
+import NotFoundPage from './pages/not-found/not-found'
+import { confirmPasswordLoader, homeLoader, loginLoader, OTPLoader, resetPasswordLoader, verifyOTPLoader } from './router/loaders'
+import { confirmPasswordAction, forgetPasswordAction, loginAction, OTPAction, registerAction, resetPasswordAction, verifyOTPAction } from './router/actions'
 
 export default function Router() {
     const router = createBrowserRouter([
@@ -19,16 +27,73 @@ export default function Router() {
                         const { default: DashboardPage } = await import("@/pages/dashboard/dashboard-page")
                         return { Component: DashboardPage }
                     },
+                    loader: homeLoader,
                 },
             ]
         },
         {
             path: '/login',
             Component: LoginPage,
+            loader: loginLoader,
+            action: loginAction
+        },
+        {
+            path: '/register',
+            lazy: async () => {
+                const { default: AuthRootLayout } = await import('@/pages/auth//auth-root-layout')
+                return { Component: AuthRootLayout }
+            },
+            children: [
+                {
+                    index: true,
+                    Component: RegisterPage,
+                    loader: loginLoader,
+                    action: registerAction
+                },
+                {
+                    path: "otp",
+                    Component: VerifyOTPPage,
+                    loader: OTPLoader,
+                    action: OTPAction
+                },
+                {
+                    path: "confirm-password",
+                    Component: ConfirmPasswordPage,
+                    loader: confirmPasswordLoader,
+                    action: confirmPasswordAction
+                }
+            ]
+        },
+        {
+            path: '/forgot-password',
+            lazy: async () => {
+                const { default: AuthRootLayout } = await import("@/pages/auth/auth-root-layout")
+                return { Component: AuthRootLayout }
+            },
+            children: [
+                {
+                    index: true,
+                    Component: ForgotPasswordPage,
+                    loader: loginLoader,
+                    action: forgetPasswordAction,
+                },
+                {
+                    path: 'verify-otp',
+                    Component: VerifyForgotOTPPage,
+                    loader: verifyOTPLoader,
+                    action: verifyOTPAction
+                },
+                {
+                    path: 'reset-password',
+                    Component: ResetPasswordPage,
+                    loader: resetPasswordLoader,
+                    action: resetPasswordAction
+                }
+            ]
         },
         {
             path: '*',
-            Component: NotFound
+            Component: NotFoundPage
         }
     ])
 
