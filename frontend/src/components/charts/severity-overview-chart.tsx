4793f48fadbf7b12a3283c70b2364058eccb1886
fisheeesh <swanphyo444@gmail.com>
2025-09-10 14:34:17 +0700

    chore: severity chart created with dummy data

---
+++ frontend/src/components/charts/severity-overview-chart.tsx
@@ -0,0 +1,70 @@
+import { Pie, PieChart } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card"
+import {
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+    type ChartConfig,
+} from "@/components/ui/chart"
+
+export const description = "A donut chart"
+
+const chartData = [
+    { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+    { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+    { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+    { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+    { browser: "other", visitors: 90, fill: "var(--color-other)" },
+]
+
+const chartConfig = {
+    visitors: {
+        label: "Visitors",
+    },
+    chrome: {
+        label: "Chrome",
+        color: "var(--chart-1)",
+    },
+    safari: {
+        label: "Safari",
+        color: "var(--chart-2)",
+    },
+    firefox: {
+        label: "Firefox",
+        color: "var(--chart-3)",
+    },
+    edge: {
+        label: "Edge",
+        color: "var(--chart-4)",
+    },
+    other: {
+        label: "Other",
+        color: "var(--chart-5)",
+    },
+} satisfies ChartConfig
+
+export function SeverityOverviewChart() {
+    return (
+        <Card className="h-full">
+            <CardHeader className="items-center pb-2">
+                <CardTitle className="text-xl md:text-2xl">Severity Overview</CardTitle>
+                <CardDescription>January - June 2024</CardDescription>
+            </CardHeader>
+            <CardContent className="h-[320px]">
+                <ChartContainer config={chartConfig} className="h-full w-full">
+                    <PieChart>
+                        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+                        <Pie data={chartData} dataKey="visitors" nameKey="browser" innerRadius="55%" outerRadius="80%" />
+                    </PieChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    )
+}
