a5527141d0c87bb744e77f57d2a02d4ea3052394
fisheeesh <swanphyo444@gmail.com>
2025-09-11 17:22:33 +0700

    feat: created logs source comparisons api route

--- backend/src/controllers/user/user-controller.ts
+++ backend/src/controllers/user/user-controller.ts
@@ -1,13 +1,18 @@
+import { endOfDay, startOfDay, subDays } from "date-fns"
 import { NextFunction, Request, Response } from "express"
 import { query, validationResult } from "express-validator"
 import { errorCodes } from "../../config/error-codes"
+import { PrismaClient } from "../../generated/prisma"
 import { getUserById } from "../../services/auth-services"
+import { getLogsOverviewFor60days, getLogsSourceComparison } from "../../services/log-services"
+import { getUserdataById } from "../../services/user-services"
 import { checkUserIfNotExist, createHttpError } from "../../utils/check"
-import { prisma } from "../../config/prisma-client"
 
 interface CustomRequest extends Request {
     userId?: number
 }
+
+const prisma = new PrismaClient()
 
 export const testUser = async (req: CustomRequest, res: Response, next: NextFunction) => {
     res.status(200).json({
@@ -21,16 +26,7 @@ export const getUserData = async (req: CustomRequest, res: Response, next: NextF
     const user = await getUserById(userId!)
     checkUserIfNotExist(user)
 
-    const data = await prisma.user.findUnique({
-        where: { id: userId! },
-        select: {
-            id: true,
-            fullName: true,
-            email: true,
-            role: true,
-            tenant: true
-        }
-    })
+    const data = await getUserdataById(user!.id)
 
     res.status(200).json({
         message: "Here is your data",
@@ -38,8 +34,7 @@ export const getUserData = async (req: CustomRequest, res: Response, next: NextF
     })
 }
 
-export const getLogs = [
-    query("tenant", "Tenant is required."),
+export const getLogsOverview = [
     async (req: CustomRequest, res: Response, next: NextFunction) => {
         const errors = validationResult(req).array({ onlyFirstError: true })
         if (errors.length > 0) return next(createHttpError({
@@ -48,10 +43,53 @@ export const getLogs = [
             code: errorCodes.invalid
         }))
 
-        const { } = req.query
+        const userId = req.userId
+        const user = await getUserById(userId!)
+        checkUserIfNotExist(user)
 
-        res.status(201).json({
+        const now = new Date();
+
+        const start = startOfDay(subDays(now, 59))
+        const end = endOfDay(now)
+
+        //? Desired format: [ { date: "2024-04-01", value: 222}, ...]
+        const result = await getLogsOverviewFor60days(user!.tenant, start, end)
+
+        res.status(200).json({
             message: 'Here is your logs.',
+            data: result
+        })
+    }
+]
+
+export const getSourceComparisons = [
+    query("duration", "Invalid Duration").trim().escape().optional(),
+    async (req: CustomRequest, res: Response, next: NextFunction) => {
+        const errors = validationResult(req).array({ onlyFirstError: true })
+        if (errors.length > 0) return next(createHttpError({
+            message: errors[0].msg,
+            status: 400,
+            code: errorCodes.invalid
+        }))
+
+        const { duration = '7' } = req.query
+        const userId = req.userId
+        const user = await getUserById(userId!)
+        checkUserIfNotExist(user)
+
+        const now = new Date();
+
+        const gap = +duration < 7 ? 6 : +duration - 1
+        console.log(gap)
+        const start = startOfDay(subDays(now, gap))
+        const end = endOfDay(now)
+
+        // ? Desired format: [{date: 'Sep 01', api: 100, ... }, ...]
+        const result = await getLogsSourceComparison(user!.tenant, start, end)
+
+        res.status(200).json({
+            message: "Here is Log's Source Comparison data.",
+            data: result
         })
     }
 ]
--- backend/src/routes/v1/user/user-routes.ts
+++ backend/src/routes/v1/user/user-routes.ts
@@ -1,10 +1,12 @@
 import express from "express"
-import { getLogs, getUserData, testUser } from "../../../controllers/user/user-controller"
+import { getLogsOverview, getSourceComparisons, getUserData, testUser } from "../../../controllers/user/user-controller"
 
 const router = express.Router()
 
 router.get("/test", testUser)
-router.get("/get-logs", getLogs)
+router.get("/logs-overview", getLogsOverview)
+router.get("/source-comparisons", getSourceComparisons)
+
 router.get("/user-data", getUserData)
 
 export default router
